72a996eed6dcffadd078d7251e4c014f695e1adf
Eduardo Pérez Rosales <420090601@pcpuma.acatlan.unam.mx>
2022-03-26 14:08:29 -0600

    Corrección de funciones
    Se corrigieron las funciones de acceso y formato y se documentaron.

---
+++ README.md
@@ -0,0 +1,18 @@
+# DesarrolloWeb EPR 01
+Repositorio de trabajos de la materia Desarrollo Web  
+
+Semestre 2022-2  
+
+Grupo 2653
+
+Pérez Rosales Eduardo  
+
+420090601
+
+## Contenido en esta rama
+
+- Actividad6
+    * login.js: Funciones que modifican el formato y acceso al sistema
+    * inicio.html: Página principal
+    * estilos.css: Bordes personalizados para los campos
+    * bootstrap-5.1.3-dist : Carpeta de estilos de bootstrap
--- login.js
+++ login.js
@@ -8,6 +8,8 @@ RETURN: Ninguno
     
 }*/
 
+/*Añade la clase edit-input al input del usuario.
+La clase edit-input da borde verde al campo*/
 function editUser(){
     try{
         var inputUser = document.getElementById("user");
@@ -18,6 +20,8 @@ function editUser(){
     }
 }
 
+/*Añade la clase edit-input al input de la contraseña.
+La clase edit-input da borde verde al campo*/
 function editPass(){
     try{
         var inputPass = document.getElementById("pass");
@@ -28,34 +32,48 @@ function editPass(){
     }
 }
 
+/*Verifica si hay valores vacíos en los campos.
+Si los hay,  quita la clase edit-input (borde verde) y agrega la clase border-input (borde rojo)
+La variable stop es un centinela que determina si hubo al menos un campo vacío*/
+
 function emptyValues(){
     try {
         var user = document.getElementById("user").value;
         var pass = document.getElementById("pass").value;
+        var stop = 1;
         if(user==""){
-            alert("Campo usuario obligatorio");
             var inputUser = document.getElementById("user");
             inputUser.classList.remove("edit-input");
             inputUser.classList.add("error-input");
+            alert("Campo usuario obligatorio");
+            stop=0;
         }
         if(pass==""){
-            alert("Campo contraseña obligatorio");
             var inputUser = document.getElementById("pass");
             inputUser.classList.remove("edit-input");
             inputUser.classList.add("error-input");
+            alert("Campo contraseña obligatorio");
+            stop=0;
         }
-        return;
+        return stop;
     } catch (error) {
         console.log(error);
     }
 }
 
+/*Ejecuta la función para determinar si hay campos vacíos. Si los hay, no verifica el acceso.
+Si no hay valores vacíos, revisa los textos introducidos y los compara con "mac" y "2022" */
+
 function setLogin(){
     try {
+        var statusLogin = emptyValues();
+        if(statusLogin==0){
+            return;
+        }
         var user = document.getElementById("user").value;
         var pass = document.getElementById("pass").value;
         if(user=="mac" && pass=="2022"){
-            alert("Bienvenido");
+            alert("¡Bienvenido!");
         }
         else{
             alert("¡Datos incorrectos!");
